cf05345e2d924ca07cfb2b7155a44799ab7381d9
Adrien Vinches <ad.vinches@gmail.com>
2022-03-28 14:01:35 -0700

    add tns resolver and tns lib

---
+++ src/libs/tns.js
@@ -0,0 +1,9 @@
+import TNS from 'tns-resolver';
+// import Config from '../config';
+
+// const endpoint = Config.ethRPCEndpoint || "https://eth-rpc-api.thetatoken.org/rpc";
+const endpoint = "https://eth-rpc-api.thetatoken.org/rpc";
+
+const tns = new TNS({ customRpcEndpoint: endpoint });
+
+export default tns;
